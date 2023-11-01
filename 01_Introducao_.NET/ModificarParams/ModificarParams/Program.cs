75e685aa6839408f62f088a114c7cc6e1beb1e0f
denervalspelizario <denervalspelizario@gmail.com>
2023-11-01 18:24:23 -0300

    estudo c# array e percorrer um array

--- 01_Introducao_.NET/ModificarParams/ModificarParams/Program.cs
+++ 01_Introducao_.NET/ModificarParams/ModificarParams/Program.cs
@@ -2,11 +2,16 @@
 Console.WriteLine("\nSoma de números inteiros\n");
 
 
-int[] valores = { 1, 2, 3 };
+int[] valores = { 1, 2, 3, 4, 5 }; // array de inteiros 
 
 
 // resultado recebe  metodo Soma com parametro valores
 var resultado = Calcular.Soma(valores);
+Console.WriteLine(resultado);
+
+var resultado02 = Calcular.Soma(1 ,2 ,3 , 4, 5);
+Console.WriteLine(resultado02);
+
 
 Console.ReadKey();
 
@@ -16,7 +21,7 @@ public class Calcular
 {
 
     // metodo Somar
-    public static int Soma(int[] numeros) // recebe array de numeros ** não definido tamanho
+    public static int Soma(params int[] numeros) // recebe array de numeros não definido tamanho
     {
         int total = 0; 
 
